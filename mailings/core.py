36520d0fcaab4235464335a2cb684ae773af46ad
jurgis <jurgis@aai-labs.com>
2022-02-06 22:51:59 +0200

    data parsing & validation

--- mailings/core.py
+++ mailings/core.py
@@ -1,10 +1,55 @@
 import csv
 
+from datetime import date
+from typing import Optional
+from pydantic.dataclasses import dataclass
+from pydantic.types import conint, constr
+
 
 def read_csv_rows(path) -> list:
     with open(path) as f:
         reader = csv.reader(f)
         return list(reader)
+
+
+current_year = date.today().year
+
+
+@dataclass
+class Birthday:
+    year: Optional[conint(ge=current_year - 100, le=current_year)]
+    month: conint(ge=1, le=12)  # constrained-int https://pydantic-docs.helpmanual.io/usage/types/#constrained-types
+    day: conint(ge=1, le=31)
+
+    @classmethod
+    def from_str(cls, val: str) -> "Birthday":
+        parts = val.split("-")
+        if len(parts) == 2:  # no year
+            return Birthday(None, *parts)
+        elif len(parts) == 3:
+            return Birthday(*parts)
+        else:
+            raise ValueError(f"can't parse Birthday from {val!r}")
+
+
+@dataclass
+class User:
+    # class User(BaseModel):
+    name: constr(strip_whitespace=True, min_length=1)
+    email: constr(strip_whitespace=True, to_lower=True, min_length=1)
+    birthday: Birthday
+
+
+def validate_data_and_convert_into_objects(data: list):
+    results = []
+    errors = []
+    for row in data:
+        # assert len(row) >= 3, "not enough values in row to unpack"
+        name, email, bday, *etc = row
+        user = User(name=name, email=email, birthday=Birthday.from_str(bday))
+        results.append(user)
+
+    return results, errors
 
 
 def process_data():
--- tests/test_mailings.py
+++ tests/test_mailings.py
@@ -2,7 +2,9 @@ from pathlib import Path
 
 import unittest
 
-from mailings.core import read_csv_rows
+from pydantic import ValidationError
+
+from mailings.core import read_csv_rows, validate_data_and_convert_into_objects, Birthday, User
 
 TESTS_ROOT_DIR = Path(__file__).resolve().parent
 DATA_PATH = TESTS_ROOT_DIR / "data"
@@ -30,3 +32,66 @@ class DataInput(unittest.TestCase):
         data = read_csv_rows(DATA_PATH / "data_with_errs.csv")
         print(data)
         assert len(data) == 15
+
+
+class DataValidation(unittest.TestCase):
+    """
+    The data file is considered valid if:
+    • it can be successfully parsed,
+    • all people have a name set,
+    • all people have an email set (however, you don't need to check if the email addresses are valid or not),
+    • each person's birthdate is a valid date (eg. no 02-30 or 01-32) in the PAST.
+    """
+
+    def test_parse_data(self):
+        data = read_csv_rows(DATA_PATH / "data.csv")
+
+        users, errors = validate_data_and_convert_into_objects(data)
+        # print(f"{users=} \n {errors=}")
+        assert len(users) == 4
+
+    def test_name_invalid_empty(self):
+        bday = Birthday(None, "12", 29)
+        with self.assertRaises(ValidationError) as cm:
+            _ = User(name="", email="la@bas.lt", birthday=bday)
+        print(cm.exception)
+        assert "name" in str(cm.exception)
+        assert "at least 1 characters" in str(cm.exception)
+
+    def test_email_invalid_empty(self):
+        bday = Birthday(None, "12", 29)
+        with self.assertRaises(ValidationError) as cm:
+            _ = User(name="A", email="", birthday=bday)
+        print(cm.exception)
+        assert "email" in str(cm.exception)
+        assert "at least 1 characters" in str(cm.exception)
+
+
+class BirthdayValidation(unittest.TestCase):
+    def test_parse_date_3parts_ok(self):
+        result = Birthday(2000, "02", 29)
+        assert result.day == 29
+        assert result.month == 2
+        assert result.year == 2000
+
+    def test_parse_date_2parts_ok(self):
+        result = Birthday(None, "02", 29)
+        print(result)
+
+    def test_month_invalid(self):
+        with self.assertRaises(ValidationError) as cm:
+            _ = Birthday(None, "13", 29)
+        assert "month" in str(cm.exception)
+        assert "limit_value=12" in str(cm.exception)
+
+    def test_day_invalid(self):
+        with self.assertRaises(ValidationError) as cm:
+            _ = Birthday(None, 10, 33)
+        assert "day" in str(cm.exception)
+        assert "limit_value=31" in str(cm.exception)
+
+    def test_year_invalid(self):
+        with self.assertRaises(ValidationError) as cm:
+            _ = Birthday(2030, 10, 20)
+        assert "year" in str(cm.exception)
+        assert "limit_value=2022" in str(cm.exception)
